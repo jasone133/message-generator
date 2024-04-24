2cc832b37e25a435c06fdaa1137d79a2525a9544
Jason Ebright <jason.ebright@verizon.net>
2024-04-24 08:45:11 -0400

    working project

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const nouns = [
-    'He', 'The boy', 'She', 'The girl', 'Mom', 'Dad', ''
+    'He', 'The boy', 'She', 'The girl', 'Mom', 'Dad'
   ]
   
   const verbs = [
-  
+    'won', 'lost', 'survived', 'overcame'
   ]
   
-  const ending = [];
+  const ending = [
+    'the race', 'the game', 'the war', 'the tournament'
+  ];
+
+  function writeMessage(first, second, third) {
+    console.log(`${first[randomWord(first.length)]} ${second[randomWord(second.length)]} ${third[randomWord(third.length)]}.`)
+  }
+
+  function randomWord(num) {
+    return Math.floor(Math.random() * num);
+  }
+
+  writeMessage(nouns, verbs, ending);
+  
+
